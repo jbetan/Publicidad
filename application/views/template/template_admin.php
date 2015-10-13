5fe3e0d1ec66081b18dc9965476729a92955f0ee
Jossue Betancourt Lopez <dark_azul100@hotmail.com>
2015-10-12 22:31:10 -0500

    prueba2

--- application/views/template/template_admin.php
+++ application/views/template/template_admin.php
@@ -6,6 +6,6 @@ $this->load->view('template/front_end/menu_admin');
 $this->load->view('template/front_end/header_s_admin');
 /*Contenido Dinamico*/
 $this->load->view('conten_dinamic/adminGral/'.$contenido);
-/*Footer*/
+/*Footer------------*/
 
 $this->load->view('template/front_end/footer_negro_admin');
